fde5d41fed9ce51b9343f4755b42d101e0539636
rizzal05-arch <rizzal05@student.uns.ac.id>
2026-01-21 10:06:08 +0700

    Fix favicon cache-busting: replace time() with md5_file() for stable versioning

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('title', 'REKA LMS') - MersifLab</title>
     
     <!-- Favicon -->
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon.png') }}?v={{ time() }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon.png') }}?v={{ file_exists(public_path('images/favicon.png')) ? md5_file(public_path('images/favicon.png')) : '1' }}">
     
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
