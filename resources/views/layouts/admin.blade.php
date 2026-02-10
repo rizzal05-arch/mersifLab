858d485f57a4cbdddfb42972bd30c4487065e896
nabkaylla <nabkaylla@student.uns.ac.id>
2026-02-10 14:38:10 +0700

    modify dashboard admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1071,7 +1071,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     
     <script>
         // Prevent back button after logout
